95ff0a7477f9735d8c613d657f3679a078fdf41c
Eduardo <201258@ids.upchiapas.edu.mx>
2021-11-07 19:55:09 -0600

    Eduardo

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,51 +1,47 @@
 import React from "react";
 
-let totalPrice = 0;
-
+let totalPay = 0;
+let view = document.getElementById('totalFinal');
 const Products = ({products, saleNote, setSaleNote, product}) =>{
 
-    const {id, code, urlImage, name, price, description, stock, amount} = products;
+    const {id, code, urlImage, name, price, description, stock, amount, total} = products;
 
-    const calculatePrice = (total) =>{
-        totalPrice = totalPrice + ( total - (totalPrice - total)) - total;
-        var total_ = 0;
-        total_ = total_ + total;
-        console.log(total_);
-    }
 
-    const addSale = (id) => {
+    const addSale = function(id) {
         let filterProduct = product.filter((products) => products.id === id);
-
         let value = true;
         var producto = [];
-        let total = 0;
+        let finalTotal = 0;
 
         if (filterProduct[0].stock>0){
-            for (var i =0; i<saleNote.length; i++){
+            for (let i =0; i<saleNote.length; i++){
                 producto.push(saleNote[i]);
 
                 if (filterProduct[0].id === saleNote[i].id){
                     filterProduct[0].stock = filterProduct[0].stock -1;
                     saleNote[i].amount = saleNote[i].amount + 1;
-                    total = saleNote[i].price * saleNote[i].amount ;
-
+                    saleNote[i].total = saleNote[i].price * saleNote[i].amount;
                     value = false;
-
                     setSaleNote(producto);
                 }
             }
             if(value){
                 filterProduct[0].stock = filterProduct[0].stock -1;
                 filterProduct[0].amount = filterProduct[0].amount +1;
-                total = filterProduct[0].price * filterProduct[0].amount;
+                finalTotal = finalTotal + filterProduct[0].price * filterProduct[0].amount;
+                filterProduct[0].total = filterProduct[0].total + finalTotal;
                 setSaleNote([...saleNote, ...filterProduct]);
 
             }
+            for (let i =0; i<saleNote.length; i++ ){
+                finalTotal = finalTotal + saleNote[i].total;
+            }
+            totalPay = finalTotal;
+            view.innerHTML = totalPay;
 
         }else {
             alert('No quedan productos disponibles')
         }
-        calculatePrice(total);
     }
 
     const deleteSale = (id) =>{
@@ -53,6 +49,11 @@ const Products = ({products, saleNote, setSaleNote, product}) =>{
         let filterNote = saleNote.filter((products) => products.id !== id);
 
         products.stock = products.stock + filterProduct[0].amount;
+
+        totalPay = totalPay - (filterProduct[0].amount * filterProduct[0].price);
+        console.log(totalPay);
+        view.innerHTML = totalPay;
+
         filterProduct[0].amount = 0;
         setSaleNote(filterNote);
     }
@@ -72,30 +73,33 @@ const Products = ({products, saleNote, setSaleNote, product}) =>{
                             <br/>
                             Stock: {stock}.
                             <br/>
-                            Precio: ${price}.
+                            Precio unidad: ${price}.
                             <br/>
                             <button type='button' onClick={() => addSale(id)}>Agregar producto</button>
                         </li>
                 ):(
                     <li>
-                        <img src={urlImage} width="100px"/>
                         <br/>
-                        {name}.
-                        <br/>
-                        {description}
-                        <br/>
-                        Cantidad: {amount}.
+                        {name}
                         <br/>
                         Precio Unidad: ${price}.
                         <br/>
+                        Cantidad: {amount}.
+                        <br/>
+                        Total: ${total}
+                        <br/>
                         <button type='button' onClick={() => deleteSale(id)}>Eliminar</button>
+                        <button type='button' onClick={() => deleteSale(id)}>CONFIRMAR</button>
                     </li>
-
                 )
                 }
+
             </ul>
+
         </div>
+
     );
+
 }
 
 export default Products;
--- src/Components/SalesNote/SaleNote.js
+++ src/Components/SalesNote/SaleNote.js
@@ -5,9 +5,12 @@ const SaleNote = ({saleNote, setSaleNote}) =>{
     return(
         <div className="saleNote">
             <h3>Nota de ventas</h3>
+            <h4>Producto.</h4> <h4>Cantidad.</h4> <h4>Precio.</h4>
             {saleNote.length ===0 ? (<p>Aun no cuenta con productos</p>) : (saleNote.map((products => <Products key={products.id} products={products} saleNote={saleNote} setSaleNote={setSaleNote} />)))}
+
         </div>
     );
+
 };
 
 export default SaleNote;
